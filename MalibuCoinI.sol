4cfbd85f49d0b4d7e246642cb45055865e7a1201
Ken Malibu <104696490+kenmalibu@users.noreply.github.com>
2022-06-26 08:50:27 +0100

    Update MalibuCoinI.sol

--- MalibuCoinI.sol
+++ MalibuCoinI.sol
@@ -5,5 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 abstract contract MalibuCoinI is IERC20, Ownable {
+         uint256 public maxSupply;
+
         function getUnclaimedMalibuCoins(address account) external view virtual returns (uint256 amount);
+        
+        function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool);
+
+        function balanceOf(address account) public view virtual override returns (uint256);
 }
